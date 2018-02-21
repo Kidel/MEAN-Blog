182ac412dd982207c8092a71d8af3438ef145cef
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2018-02-21 16:14:30 +0100

    changed appserver default port because of reasons

--- ApplicationServer/server.js
+++ ApplicationServer/server.js
@@ -23,7 +23,7 @@ app.get('*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '8080';
+const port = process.env.PORT || '8888';
 app.set('port', port);
 
 /**
